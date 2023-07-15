29d97dc42a0faae44606522df6e344402cdfeabd
s2cocos2 <yewon0309@naver.com>
2023-07-15 23:19:08 +0900

    fix : 데이터 구조 변경

--- src/main/java/com/anabada/anabada/item/model/response/ItemFindResponse.java
+++ src/main/java/com/anabada/anabada/item/model/response/ItemFindResponse.java
@@ -6,6 +6,7 @@ import com.anabada.anabada.item.model.type.ItemStatus;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @Getter
@@ -13,6 +14,7 @@ public class ItemFindResponse {
     private Long id;
     private String itemName;
     private String itemContent;
+    private String itemOneContent;
     private String tradingPosition;
     private String tradingItem;
     private String img;
@@ -23,11 +25,15 @@ public class ItemFindResponse {
         this.id = item.getId();
         this.itemName = item.getItemName();
         this.itemContent = item.getItemContent();
+        this.itemOneContent = item.getItemOneContent();
         this.tradingPosition = item.getTradingPosition();
         this.tradingItem = item.getTradingItem();
-        this.img = item.getImg();
-        this.imgList = item.getImgList();
+        this.img = "https://anabada123.s3.ap-northeast-2.amazonaws.com/" + item.getImg();
+        this.imgList = new ArrayList<>();
+        for (String imgUrl : item.getImgList()) {
+            String s = "https://anabada123.s3.ap-northeast-2.amazonaws.com/" + imgUrl;
+            this.imgList.add(s);
+        }
         this.day = item.getCreatedAt();
     }
-
 }
