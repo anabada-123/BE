1a031c5a7c1f787fbe24b6ad4e444f77ad9dfa7f
Domae-back-end <ty_ty123@naver.com>
2023-07-19 19:22:43 +0900

    fix : CORS 수정

--- src/main/java/com/anabada/anabada/global/config/WebMvcConfig.java
+++ src/main/java/com/anabada/anabada/global/config/WebMvcConfig.java
@@ -10,9 +10,9 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
+                .allowedOrigins("*")
                 .allowedMethods("*")
-                .allowedHeaders("*")
-                .allowCredentials(true);
+                .allowedHeaders("*");
     }
 
 }
