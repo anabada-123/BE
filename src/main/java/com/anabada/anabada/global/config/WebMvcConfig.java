49459a5bed7eba9d614f622b079b572cd9981d14
Domae-back-end <ty_ty123@naver.com>
2023-07-19 17:27:16 +0900

    errpr : CORS 에러

--- src/main/java/com/anabada/anabada/global/config/WebMvcConfig.java
+++ src/main/java/com/anabada/anabada/global/config/WebMvcConfig.java
@@ -10,10 +10,10 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOriginPatterns("*")
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(true);
-        
     }
+
 }
